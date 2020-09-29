a7767ea9bcbe0de74d67333bb56484d244d34e56
blackymax <maxmin3007@gmail.com>
2020-09-29 19:32:25 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let alpha = str;
+  let beta
+  
+   for(let i = 0;i<bracketsConfig.length; i++){ 
+       let vs=bracketsConfig[i].join('')
+      beta = alpha.replace(vs,"")
+      alpha = beta
+   }
+   if(alpha === ''){
+      return true}
+      else if(alpha===str){
+       return false
+      }else{
+          return check(alpha,bracketsConfig)
+      }
 }
